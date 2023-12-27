98fb70cb279209e93343589a6254fe65891656e9
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2023-12-27 10:25:05 +0530

    Add init and quit for video capture

--- src/image_capture.py
+++ src/image_capture.py
@@ -1,13 +1,27 @@
-from multiprocessing import Process, Event, Value
+from multiprocessing import Process, Event
+
+import cv2
 
 
 class ImageCaptureProcess(Process):
     def __init__(self, device, capture_signal, quit_event):
         super().__init__()
-        pass
+        self.device = device
+        self.capture_signal = capture_signal
+        self.quit_event = quit_event
+        self.cap = None
 
     def run(self):
-        pass
+        self.cap = cv2.VideoCapture()
+
+        while not self.quit_event.is_set():
+            ret, frame = self.cap.read()
+            pass
+
+        self.quit()
 
     def quit(self):
-        pass
+        if self.cap:
+            self.cap.release()
+        self.cap = None
+        self.quit_event.set()
