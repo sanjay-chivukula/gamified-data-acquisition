b15de58d8a9baa04188b7585d78aad17a3c6af65
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2024-01-01 22:41:21 +0530

    Add image label data pojo

--- src/data_models.py
+++ src/data_models.py
@@ -2,12 +2,20 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Tuple
 
-import numpy.typing as npt
+import numpy as np
 
 
 @dataclass
 class CaptureSignalPayload:
     is_capture: bool
     target_pos: Tuple[int, int]
-    frame: npt.NDArray
+    frame: np.ndarray
     timestamp: datetime
+
+
+@dataclass
+class ImageLabelData:
+    image: np.ndarray
+    target_pos: Tuple
+    face_pos: Tuple
+    face_bounds: Tuple
